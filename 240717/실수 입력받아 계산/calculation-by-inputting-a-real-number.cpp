fad9d5328d1e6791c5f3ce68533c09a4d1258e48
dudgus54 <123707193+kainel54@users.noreply.github.com>
2024-07-17 23:37:38 +0900

    Title: 실수 입력받아 계산, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
+++ 240717/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    float a,b;
-    cin>> a>>b;
-    cout>>fixed;
+    double a,b;
+    cin>>a>>b;
+    cout<<fixed;
     cout.precision(2);
     cout<<a+b;
     return 0;
